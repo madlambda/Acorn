8a32df15837ae65e1b793d3892d86704b5067a41
Tiago de Bem Natel de Moura <t.nateldemoura@f5.com>
2020-04-20 00:39:08 +0100

    style: plan9

--- oak/bin.h
+++ oak/bin.h
@@ -6,10 +6,16 @@
 #ifndef _OAK_BIN_H_
 
 
-ssize_t uleb128_encode(u64 v, u8 *begin, u8 *end);
-ssize_t sleb128_encode(i64 v, u8 *begin, u8 *end);
-ssize_t uleb128_decode(const u8 *begin, const u8 *end, u64 *res);
-ssize_t sleb128_decode(const u8 *begin, const u8 *end, i64 *res);
+#define uencode(v, begin, end)      uleb128encode(v, begin, end)
+#define sencode(v, begin, end)      sleb128encode(v, begin, end)
+#define udecode(begin, end, res)    uleb128decode(begin, end, res)
+#define sdecode(begin, end, res)    sleb128decode(begin, end, res)
+
+
+ssize_t uleb128encode(u64 v, u8 *begin, u8 *end);
+ssize_t sleb128encode(i64 v, u8 *begin, u8 *end);
+ssize_t uleb128decode(const u8 *begin, const u8 *end, u64 *res);
+ssize_t sleb128decode(const u8 *begin, const u8 *end, i64 *res);
 
 
 #endif /* _OAK_BIN_H_ */
