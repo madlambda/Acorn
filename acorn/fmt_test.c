f3d3930ff0169b8aa485aa7b85d27732946bd875
Tiago de Bem Natel de Moura <tiago4orion@gmail.com>
2020-04-25 23:29:13 +0100

    Introduce custom formatters. (#17)

--- acorn/fmt_test.c
+++ acorn/fmt_test.c
@@ -10,7 +10,9 @@
 
 static u8 test_fmtstring();
 static u8 test_fmtint();
+static u8 test_fmtchar();
 static u8 test_invalidverbs();
+static u8 test_customprinter();
 
 
 int
@@ -24,7 +26,15 @@ main()
         return 1;
     }
 
+    if (slow(test_fmtchar() != OK)) {
+        return 1;
+    }
+
     if (slow(test_invalidverbs() != OK)) {
+        return 1;
+    }
+
+    if (slow(test_customprinter() != OK)) {
         return 1;
     }
 
@@ -80,6 +90,46 @@ test_fmtstring()
     free(res);
 
     return OK;
+}
+
+
+static u8
+test_fmtchar()
+{
+    u32     len;
+    char    *p;
+    String  *res;
+    char    tmp[1024];
+
+    res = cfmt("%c%c%c", 'i', '4', 'k');
+    if (slow(res == NULL)) {
+        goto error;
+    }
+
+    if (slow(!cstringcmp(res, "i4k"))) {
+        goto error;
+    }
+
+    free(res);
+
+    return OK;
+
+error:
+
+    if (res != NULL) {
+        p = copy(tmp, res->start, res->len);
+        *p = '\0';
+        len = res->len;
+        free(res);
+        p = tmp;
+    } else {
+        len = 0;
+        p = "res is NULL";
+    }
+
+    printf("failed: %s (len: %d)\n", p, len);
+
+    return ERR;
 }
 
 
@@ -149,6 +199,90 @@ test_invalidverbs()
     }
 
     free(res);
+
+    return OK;
+}
+
+
+static u8
+mydumbfmt1(String **buf, u8 ** unused(format), void * unused(val))
+{
+    *buf = appendcstr(*buf, "dumb formatter");
+    return OK;
+}
+
+
+typedef struct {
+    char  a;
+    char  b;
+} Custom;
+
+
+static u8
+mydumbfmt2(String **buf, u8 ** unused(format), void *val)
+{
+    Custom  *c;
+
+    c = (Custom *) val;
+
+    *buf = appendc(*buf, 10,
+                   'a', ':', ' ', c->a, ',', ' ', 'b', ':', ' ', c->b);
+    return OK;
+}
+
+
+static u8
+test_customprinter()
+{
+    String   *s;
+    Custom   custom;
+    Printer  *printer;
+
+    printer = newprinter();
+    if (slow(printer == NULL)) {
+        return ERR;
+    }
+
+    s = pcfmt(printer, "a%sb", "test");
+    if (slow(s == NULL)) {
+        return ERR;
+    }
+
+    if (slow(!cstringcmp(s, "atestb"))) {
+        return ERR;
+    }
+
+    free(s);
+
+    pfmtadd(printer, 'D', mydumbfmt1);
+
+    s = pcfmt(printer, "My %D.", "ignored");
+    if (slow(s == NULL)) {
+        return ERR;
+    }
+
+    if (slow(!cstringcmp(s, "My dumb formatter."))) {
+        return ERR;
+    }
+
+    free(s);
+
+    pfmtadd(printer, 'C', mydumbfmt2);
+
+    custom.a = 'Z';
+    custom.b = 'Y';
+
+    s = pcfmt(printer, "Values: %C", &custom);
+    if (slow(s == NULL)) {
+        return ERR;
+    }
+
+    if (slow(!cstringcmp(s, "Values: a: Z, b: Y"))) {
+        return ERR;
+    }
+
+    free(s);
+    free(printer);
 
     return OK;
 }
