5b5a5d5f274244e49b3e9f915498dd6ea25868a0
i4k <tiago4orion@gmail.com>
2020-04-27 23:26:17 +0000

    remove dublicate code in tests

--- acorn/fmt_test.c
+++ acorn/fmt_test.c
@@ -15,6 +15,10 @@ typedef struct {
     const char  *format;
     const char  *sprintf;
 
+    u8          bits;
+    u8          sign;
+
+    const char  *want;
     union {
         int     ival;
         i8      i8val;
@@ -34,18 +38,7 @@ typedef u8 (*testfn)();
 
 static u8 test_fmtstring();
 static u8 test_fmtint();
-static u8 test_fmti8();
-static u8 test_fmti16();
-static u8 test_fmti32();
-static u8 test_fmti64();
-static u8 test_fmtu8();
-static u8 test_fmtu16();
-static u8 test_fmtu32();
-static u8 test_fmtu64();
 static u8 test_fmtchar();
-
-static u8 test_fmthex();
-static u8 test_fmthexu64();
 static u8 test_invalidverbs();
 static u8 test_customprinter();
 
@@ -54,269 +47,339 @@ static u8 assertstr(String *got, char *want);
 
 static const Testcaseint  inttests[] = {
     {
-        .format = "%d",
-        /* uses the same format for sprintf */
-        .u.ival = 0,
+        .format     = "%d",
+        .sprintf    = "%d",
+        .u.ival     = 0,
+        .want       = "0",
     },
     {
-        .format = "%d",
-        .u.ival    = -1,
+        .format     = "%d",
+        .sprintf    = "%d",
+        .u.ival     = -1,
+        .want       = "-1",
     },
     {
-        .format = "%da",
-        .u.ival    = 10000000,
+        .format     = "%da",
+        .sprintf    = "%da",
+        .u.ival     = 10000000,
+        .want       = "10000000a",
     },
     {
-        .format = "%d",
-        .u.ival    = INT32_MIN,
+        .format     = "%d",
+        .sprintf    = "%d",
+        .u.ival     = INT32_MIN,
+        .want       = "-2147483648",
     },
     {
-        .format = "AI(Jijaijaankan%dnkjsdisahd9d",
-        .u.ival    = INT32_MIN,
+        .format     = "AI(Jijaijaankan%dnkjsdisahd9d",
+        .sprintf    = "AI(Jijaijaankan%dnkjsdisahd9d",
+        .u.ival     = INT32_MIN,
+        .want       = "AI(Jijaijaankan-2147483648nkjsdisahd9d",
     },
-};
-
-
-/* signed integers */
-
-static const Testcaseint  i8tests[] = {
     {
+        .bits       = 8,
+        .sign       = 1,
         .format     = "%d(i8)",
         .sprintf    = "%"PRIi8,
         .u.i8val    = 0,
+        .want       = "0",
     },
     {
+        .bits       = 8,
+        .sign       = 1,
         .format     = "%d(i8)",
         .sprintf    = "%"PRIi8,
         .u.i8val    = 100,
+        .want       = "100",
     },
     {
+        .bits       = 8,
+        .sign       = 1,
         .format     = "%d(i8)",
         .sprintf    = "%"PRIi8,
         .u.i8val    = INT8_MAX,
+        .want       = "127",
     },
     {
+        .bits       = 8,
+        .sign       = 1,
         .format     = "%d(i8)",
         .sprintf    = "%"PRIi8,
         .u.i8val    = INT8_MIN,
+        .want       = "-128",
     },
-};
-
-
-static const Testcaseint  i16tests[] = {
     {
+        .bits       = 16,
+        .sign       = 1,
         .format     = "%d(i16)",
         .sprintf    = "%"PRIi16,
-        .u.u16val   = 0,
+        .u.i16val   = 0,
+        .want       = "0",
     },
     {
+        .bits       = 16,
+        .sign       = 1,
         .format     = "%d(i16)",
         .sprintf    = "%"PRIi16,
         .u.i16val   = INT16_MAX,
+        .want       = "32767",
     },
     {
+        .bits       = 16,
+        .sign       = 1,
         .format     = "%d(i16)",
         .sprintf    = "%"PRIi16,
         .u.i16val   = INT16_MIN,
+        .want       = "-32768",
     },
     {
+        .bits       = 16,
+        .sign       = 1,
         .format     = "%d(i16)test",
         .sprintf    = "%"PRIi16"test",
         .u.i16val   = 255,
+        .want       = "255test",
     },
-};
-
-
-static const Testcaseint  i32tests[] = {
     {
+        .bits       = 32,
+        .sign       = 1,
         .format     = "%d(i32)",
         .sprintf    = "%"PRIi32,
         .u.i32val   = 0,
+        .want       = "0",
     },
     {
+        .bits       = 32,
+        .sign       = 1,
         .format     = "%d(i32)",
         .sprintf    = "%"PRIi32,
         .u.i32val   = INT32_MAX,
+        .want       = "2147483647",
     },
     {
+        .bits       = 32,
+        .sign       = 1,
         .format     = "%d(i32)",
         .sprintf    = "%"PRIi32,
         .u.i32val   = INT32_MIN,
+        .want       = "-2147483648",
     },
     {
+        .bits       = 32,
+        .sign       = 1,
         .format     = "%d(i32)test",
         .sprintf    = "%"PRIi32"test",
         .u.i32val   = 255,
+        .want       = "255test",
     },
     {
+        .bits       = 32,
+        .sign       = 1,
         .format     = "%d(i32)",
         .sprintf    = "%"PRIi32,
         .u.i32val   = 0xffff,
+        .want       = "65535",
     },
-};
-
-
-static const Testcaseint  i64tests[] = {
     {
+        .bits       = 64,
+        .sign       = 1,
         .format     = "%d(i64)",
         .sprintf    = "%"PRIi64,
         .u.i64val   = 0,
+        .want       = "0",
     },
     {
+        .bits       = 64,
+        .sign       = 1,
         .format     = "%d(i64)",
         .sprintf    = "%"PRIi64,
         .u.i64val   = INT64_MAX,
+        .want       = "9223372036854775807",
     },
     {
+        .bits       = 64,
+        .sign       = 1,
         .format     = "%d(i64)test",
         .sprintf    = "%"PRIi64"test",
         .u.i64val   = INT64_MAX,
+        .want       = "9223372036854775807test",
     },
     {
+        .bits       = 64,
+        .sign       = 1,
         .format     = "%d(i64)test",
         .sprintf    = "%"PRIi64"test",
         .u.i64val   = INT64_MIN,
+        .want       = "-9223372036854775808test",
     },
     {
+        .bits       = 64,
+        .sign       = 1,
         .format     = "%d(i64)",
         .sprintf    = "%"PRIi64,
         .u.i64val   = 0xff,
+        .want       = "255",
     },
-};
-
-
-/* unsigned integers */
-
-static const Testcaseint  u8tests[] = {
     {
+        .bits       = 8,
         .format     = "%d(u8)",
         .sprintf    = "%"PRIu8,
         .u.u8val    = 0,
+        .want       = "0",
     },
     {
+        .bits       = 8,
         .format     = "%d(u8)",
         .sprintf    = "%"PRIu8,
         .u.u8val    = INT8_MAX,
+        .want       = "127",
     },
     {
+        .bits       = 8,
         .format     = "%d(u8)test",
         .sprintf    = "%"PRIu8"test",
         .u.u8val    = UINT8_MAX,
+        .want       = "255test",
     },
     {
+        .bits       = 8,
         .format     = "%d(u8)",
         .sprintf    = "%"PRIu8,
         .u.u8val    = 0xff,
+        .want       = "255",
     },
-};
-
-
-static const Testcaseint  u16tests[] = {
     {
+        .bits       = 16,
         .format     = "%d(u16)",
         .sprintf    = "%"PRIu16,
         .u.u16val   = 0,
+        .want       = "0",
     },
     {
+        .bits       = 16,
         .format     = "%d(u16)",
         .sprintf    = "%"PRIu16,
         .u.u16val   = INT16_MAX,
+        .want       = "32767",
     },
     {
+        .bits       = 16,
         .format     = "%d(u16)test",
         .sprintf    = "%"PRIu16"test",
         .u.u16val   = UINT16_MAX,
+        .want       = "65535test",
     },
     {
+        .bits       = 16,
         .format     = "%d(u16)",
         .sprintf    = "%"PRIu16,
         .u.u16val   = 0xffff,
+        .want       = "65535",
     },
-};
-
-
-static const Testcaseint  u32tests[] = {
     {
+        .bits       = 32,
         .format     = "%d(u32)",
         .sprintf    = "%"PRIu32,
         .u.u32val   = 0,
+        .want       = "0",
     },
     {
+        .bits       = 32,
         .format     = "%d(u32)",
         .sprintf    = "%"PRIu32,
         .u.u32val   = INT32_MAX,
+        .want       = "2147483647",
     },
     {
+        .bits       = 32,
         .format     = "%d(u32)test",
         .sprintf    = "%"PRIu32"test",
         .u.u32val   = UINT32_MAX,
+        .want       = "4294967295test",
     },
     {
+        .bits       = 32,
         .format     = "%d(u16)",
         .sprintf    = "%"PRIu32,
         .u.u32val   = 0xffff,
+        .want       = "65535",
     },
-};
-
-
-static const Testcaseint  u64tests[] = {
     {
+        .bits       = 64,
         .format     = "%d(u64)",
         .sprintf    = "%"PRIu64,
         .u.u64val   = 0,
+        .want       = "0",
     },
     {
+        .bits       = 64,
         .format     = "%d(u64)",
         .sprintf    = "%"PRIu64,
         .u.u64val   = INT64_MAX,
+        .want       = "9223372036854775807",
     },
     {
+        .bits       = 64,
         .format     = "%d(u64)test",
         .sprintf    = "%"PRIu64"test",
         .u.u64val   = UINT64_MAX,
+        .want       = "18446744073709551615test",
     },
     {
+        .bits       = 64,
         .format     = "%d(u64)",
         .sprintf    = "%"PRIu64,
         .u.u64val   = 0xff,
+        .want       = "255",
     },
-};
-
-
-static const Testcaseint  hextests[] = {
     {
+        .bits       = 32,
         .format     = "%x",
         .sprintf    = "%x",
         .u.ival     = 255,
+        .want       = "ff",
     },
     {
+        .bits       = 32,
         .format     = "%x",
         .sprintf    = "%x",
         .u.ival     = 0xa,
+        .want       = "a",
     },
     {
+        .bits       = 32,
         .format     = "%x",
         .sprintf    = "%x",
-        .u.ival     = 0xa11a,
+        .u.i32val   = 0xa11a,
+        .want       = "a11a",
     },
-};
-
-
-static const Testcaseint  hexu64tests[] = {
     {
+        .bits       = 64,
         .format     = "%x(u64)",
         .sprintf    = "%"PRIx64,
         .u.u64val   = UINT64_MAX,
+        .want       = "ffffffffffffffff",
     },
     {
+        .bits       = 64,
         .format     = "%x",
         .sprintf    = "%"PRIx64,
-        .u.ival     = 0xa,
+        .u.i64val   = 0xa,
+        .want       = "a",
     },
     {
+        .bits       = 64,
         .format     = "%x",
         .sprintf    = "%"PRIx64,
-        .u.ival     = 0xa11a,
+        .u.i64val   = 0xa11a,
+        .want       = "a11a",
+    },
+    {
+        .bits       = 64,
+        .format     = "0x%x",
+        .sprintf    = "0x%"PRIx64,
+        .u.i64val   = 0xa11a,
+        .want       = "0xa11a",
     },
 };
 
@@ -331,20 +394,7 @@ main()
         test_invalidverbs,
         test_fmtchar,
         test_fmtstring,
-
         test_fmtint,
-        test_fmtu8,
-        test_fmtu16,
-        test_fmtu32,
-        test_fmtu64,
-        test_fmti8,
-        test_fmti16,
-        test_fmti32,
-        test_fmti64,
-
-        test_fmthex,
-        test_fmthexu64,
-
         test_customprinter,
     };
 
@@ -418,249 +468,65 @@ test_fmtint()
         tc = &inttests[i];
 
         memset(want, 0, sizeof(want));
-        sprintf(want, tc->format, tc->u.ival);
 
-        ret = assertstr(cfmt(tc->format, tc->u.ival), want);
-        if (slow(ret != OK)) {
+        switch (tc->bits) {
+        case 0:
+            sprintf(want, tc->sprintf, tc->u.ival);
+            ret = assertstr(cfmt(tc->format, tc->u.ival), want);
+            break;
+
+        case 8:
+            if (tc->sign) {
+                sprintf(want, tc->sprintf, tc->u.i8val);
+                ret = assertstr(cfmt(tc->format, tc->u.i8val), want);
+            } else {
+                sprintf(want, tc->sprintf, tc->u.u8val);
+                ret = assertstr(cfmt(tc->format, tc->u.u8val), want);
+            }
+            break;
+
+        case 16:
+            if (tc->sign) {
+                sprintf(want, tc->sprintf, tc->u.i16val);
+                ret = assertstr(cfmt(tc->format, tc->u.i16val), want);
+            } else {
+                sprintf(want, tc->sprintf, tc->u.u16val);
+                ret = assertstr(cfmt(tc->format, tc->u.u16val), want);
+            }
+            break;
+
+        case 32:
+            if (tc->sign) {
+                sprintf(want, tc->sprintf, tc->u.i32val);
+                ret = assertstr(cfmt(tc->format, tc->u.i32val), want);
+            } else {
+                sprintf(want, tc->sprintf, tc->u.u32val);
+                ret = assertstr(cfmt(tc->format, tc->u.u32val), want);
+            }
+            break;
+
+        case 64:
+            if (tc->sign) {
+                sprintf(want, tc->sprintf, tc->u.i64val);
+                ret = assertstr(cfmt(tc->format, tc->u.i64val), want);
+            } else {
+                sprintf(want, tc->sprintf, tc->u.u64val);
+                ret = assertstr(cfmt(tc->format, tc->u.u64val), want);
+            }
+            break;
+
+        default:
+            printf("invalid size: %d", tc->bits);
             return ERR;
         }
-    }
 
-    return OK;
-}
+        if (slow(strcmp(want, tc->want) != 0)) {
+            printf("unexpected libc sprintf \"%s\", want \"%s\"\n", want,
+                   tc->want);
 
-
-static u8
-test_fmtu8()
-{
-    u8                 ret;
-    u32                i;
-    const Testcaseint  *tc;
-    char               want[256];
-
-    for (i = 0; i < nitems(u8tests); i++) {
-        tc = &u8tests[i];
-
-        memset(want, 0, sizeof(want));
-        sprintf(want, tc->sprintf, tc->u.u8val);
-
-        ret = assertstr(cfmt(tc->format, tc->u.u8val), want);
-        if (slow(ret != OK)) {
             return ERR;
         }
-    }
 
-    return OK;
-}
-
-
-static u8
-test_fmtu16()
-{
-    u8                 ret;
-    u32                i;
-    const Testcaseint  *tc;
-    char               want[256];
-
-    for (i = 0; i < nitems(u16tests); i++) {
-        tc = &u16tests[i];
-
-        memset(want, 0, sizeof(want));
-        sprintf(want, tc->sprintf, tc->u.u16val);
-
-        ret = assertstr(cfmt(tc->format, tc->u.u16val), want);
-        if (slow(ret != OK)) {
-            return ERR;
-        }
-    }
-
-    return OK;
-}
-
-
-static u8
-test_fmtu32()
-{
-    u8                 ret;
-    u32                i;
-    const Testcaseint  *tc;
-    char               want[256];
-
-    for (i = 0; i < nitems(u32tests); i++) {
-        tc = &u32tests[i];
-
-        memset(want, 0, sizeof(want));
-        sprintf(want, tc->sprintf, tc->u.u32val);
-
-        ret = assertstr(cfmt(tc->format, tc->u.u32val), want);
-        if (slow(ret != OK)) {
-            return ERR;
-        }
-    }
-
-    return OK;
-}
-
-
-static u8
-test_fmtu64()
-{
-    u8                 ret;
-    u32                i;
-    const Testcaseint  *tc;
-    char               want[256];
-
-    for (i = 0; i < nitems(u64tests); i++) {
-        tc = &u64tests[i];
-
-        memset(want, 0, sizeof(want));
-        sprintf(want, tc->sprintf, tc->u.u64val);
-
-        ret = assertstr(cfmt(tc->format, tc->u.u64val), want);
-        if (slow(ret != OK)) {
-            return ERR;
-        }
-    }
-
-    return OK;
-}
-
-
-static u8
-test_fmti8()
-{
-    u8                 ret;
-    u32                i;
-    const Testcaseint  *tc;
-    char               want[256];
-
-    for (i = 0; i < nitems(i8tests); i++) {
-        tc = &i8tests[i];
-
-        memset(want, 0, sizeof(want));
-        sprintf(want, tc->sprintf, tc->u.i8val);
-
-        ret = assertstr(cfmt(tc->format, tc->u.i8val), want);
-        if (slow(ret != OK)) {
-            return ERR;
-        }
-    }
-
-    return OK;
-}
-
-
-static u8
-test_fmti16()
-{
-    u8                 ret;
-    u32                i;
-    const Testcaseint  *tc;
-    char               want[256];
-
-    for (i = 0; i < nitems(i16tests); i++) {
-        tc = &i16tests[i];
-
-        memset(want, 0, sizeof(want));
-        sprintf(want, tc->sprintf, tc->u.i16val);
-
-        ret = assertstr(cfmt(tc->format, tc->u.i16val), want);
-        if (slow(ret != OK)) {
-            return ERR;
-        }
-    }
-
-    return OK;
-}
-
-
-static u8
-test_fmti32()
-{
-    u8                 ret;
-    u32                i;
-    const Testcaseint  *tc;
-    char               want[256];
-
-    for (i = 0; i < nitems(i32tests); i++) {
-        tc = &i32tests[i];
-
-        memset(want, 0, sizeof(want));
-        sprintf(want, tc->sprintf, tc->u.i32val);
-
-        ret = assertstr(cfmt(tc->format, tc->u.i32val), want);
-        if (slow(ret != OK)) {
-            return ERR;
-        }
-    }
-
-    return OK;
-}
-
-
-static u8
-test_fmti64()
-{
-    u8                 ret;
-    u32                i;
-    const Testcaseint  *tc;
-    char               want[256];
-
-    for (i = 0; i < nitems(i64tests); i++) {
-        tc = &i64tests[i];
-
-        memset(want, 0, sizeof(want));
-        sprintf(want, tc->sprintf, tc->u.i64val);
-
-        ret = assertstr(cfmt(tc->format, tc->u.i64val), want);
-        if (slow(ret != OK)) {
-            return ERR;
-        }
-    }
-
-    return OK;
-}
-
-
-static u8
-test_fmthex()
-{
-    u8                 ret;
-    u32                i;
-    const Testcaseint  *tc;
-    char               want[256];
-
-    for (i = 0; i < nitems(hextests); i++) {
-        tc = &hextests[i];
-
-        memset(want, 0, sizeof(want));
-        sprintf(want, tc->sprintf, tc->u.ival);
-
-        ret = assertstr(cfmt(tc->format, tc->u.ival), want);
-        if (slow(ret != OK)) {
-            return ERR;
-        }
-    }
-
-    return OK;
-}
-
-
-static u8
-test_fmthexu64()
-{
-    u8                 ret;
-    u32                i;
-    const Testcaseint  *tc;
-    char               want[256];
-
-    for (i = 0; i < nitems(hexu64tests); i++) {
-        tc = &hexu64tests[i];
-
-        memset(want, 0, sizeof(want));
-        sprintf(want, tc->sprintf, tc->u.u64val);
-
-        ret = assertstr(cfmt(tc->format, tc->u.u64val), want);
         if (slow(ret != OK)) {
             return ERR;
         }
